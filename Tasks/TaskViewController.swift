8f783224c5386f4e23207ce30d06815d04234c4a
Jcole429 <Justincole29@hotmail.com>
2020-11-21 14:43:07 -0500

    initial commit

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by Justin Cole on 11/20/20.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+        
+//        let newCount = count - 1;
+//        
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentposition)")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
